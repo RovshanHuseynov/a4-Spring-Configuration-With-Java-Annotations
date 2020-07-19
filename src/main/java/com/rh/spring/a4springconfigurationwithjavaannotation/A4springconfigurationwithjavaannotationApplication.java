b858be7fcfa78594af4e43295ac2ab8b586ff633
RovshanHuseynov <rovsen.huseyn.oglu@gmail.com>
2020-07-19 18:43:57 +0400

    Initial commit

---
+++ src/main/java/com/rh/spring/a4springconfigurationwithjavaannotation/A4springconfigurationwithjavaannotationApplication.java
@@ -0,0 +1,11 @@
+package com.rh.spring.a4springconfigurationwithjavaannotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A4springconfigurationwithjavaannotationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(A4springconfigurationwithjavaannotationApplication.class, args);
+    }
+}
---
+++ src/test/java/com/rh/spring/a4springconfigurationwithjavaannotation/A4springconfigurationwithjavaannotationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rh.spring.a4springconfigurationwithjavaannotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A4springconfigurationwithjavaannotationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
